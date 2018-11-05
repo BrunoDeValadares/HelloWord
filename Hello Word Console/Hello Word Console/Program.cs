35340554541603a144b7bce2a923a3ee752d7b07
brunovaladares <b_valadares@yahoo.com.br>
2018-11-05 21:07:19 -0200

    Primeiro commit

---
+++ Hello Word Console/Hello Word Console/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_Word_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exemplo do uso do GitHub");
+            Console.ReadKey();
+        }
+    }
+}
